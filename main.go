186837f59e8f8bd03ee39f5f9e748a0a3dce66a0
Mehmet Ali Baykara <mab.baykara@gmail.com>
2023-05-19 08:24:56 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,68 +2,76 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/gorilla/mux"
 )
 
+// User represents a user object
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+var users []User
+
 func main() {
-	path := mux.NewRouter().StrictSlash(true)
-	v := os.Getenv("VERSION")
-	path.HandleFunc("/", home).Methods(http.MethodGet)
-	path.HandleFunc("/createUser", createUser).Methods(http.MethodPost)
-	path.HandleFunc("/users", getUsers).Methods(http.MethodGet)
-	fmt.Printf("Server is running with version %s on port 8090", v)
-	log.Fatal(http.ListenAndServe(":8090", path))
-
+	http.HandleFunc("/users", getUsersHandler)
+	http.HandleFunc("/createUsers", createUserHandler)
+	http.HandleFunc("/deleteUser", deleteUserHandler)
+	log.Println("Server is running...")
+	log.Fatal(http.ListenAndServe(":8090", nil))
 }
 
-type USER struct {
-	ID   string `json:"ID"`
-	Name string `json:"Name"`
-	Age  int    `json:"Age"`
-}
+// getUsersHandler handles the GET request for /users endpoint
+func getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
-type allUsers []USER
-
-var users = allUsers{
-	{
-		ID:   "1",
-		Name: "Lenovo",
-		Age:  3,
-	},
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to homepage!")
-}
-
-func createUser(w http.ResponseWriter, r *http.Request) {
-	var newEvent USER
-	reqBody, err := ioutil.ReadAll(r.Body)
+	jsonData, err := json.Marshal(users)
 	if err != nil {
-		fmt.Fprintf(w, "Enter valid data")
-	}
-
-	json.Unmarshal(reqBody, &newEvent)
-	err = json.Unmarshal(reqBody, &newEvent)
-	if err != nil {
-		fmt.Fprintf(w, "unvalid data")
+		http.Error(w, "Failed to retrieve users", http.StatusInternalServerError)
 		return
 	}
-	users = append(users, newEvent)
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "new user added successfully!\n")
 
-	json.NewEncoder(w).Encode(newEvent)
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData)
 }
 
-func getUsers(w http.ResponseWriter, r *http.Request) {
+// createUserHandler handles the POST request for /createUsers endpoint
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		http.Error(w, "Invalid user data", http.StatusBadRequest)
+		return
+	}
 
-	fmt.Println("List all users")
-	json.NewEncoder(w).Encode(users)
+	newUser.ID = len(users) + 1
+	users = append(users, newUser)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("User created successfully"))
+}
+
+// deleteUserHandler handles the DELETE request for /deleteUser endpoint
+func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.URL.Query().Get("username")
+
+	if username == "" {
+		// Remove the last user if no username is provided
+		if len(users) > 0 {
+			users = users[:len(users)-1]
+		}
+	} else {
+		// Find and remove the user by username
+		for i, user := range users {
+			if user.Username == username {
+				users = append(users[:i], users[i+1:]...)
+				break
+			}
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("User deleted successfully"))
 }
